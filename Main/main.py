23497e7d1283ea5fba3964f5706256752d4ad5be
Ritesh1069 <riteshmmaurya2004@gmail.com>
2024-02-07 00:28:30 +0530

    updated

--- Main/main.py
+++ Main/main.py
@@ -1,7 +1,10 @@
 import csv
 import facebook
+import pandas as pd
 
 def convert_to_csv(profile):
+    # Get profile link
+    profile_link = profile.get('link', '')
     # Extract necessary fields from the profile JSON object
     fields = ['first_name', 'gender', 'birthday', 'email', 'profile_link', 'categories']
     likes = profile.get('likes', {}).get('data', [])  # Extract 'likes' data
@@ -23,13 +26,11 @@ def convert_to_csv(profile):
         set1 += (category,)
     else:
         pass
-
-    # Get profile link
-    profile_link = profile.get('link', '')
     # Write data to a CSV file
     with open('profile_dataNewest.csv', 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
+        
         row = {
             'first_name': profile.get('first_name', ''),
             'gender': profile.get('gender', ''),
@@ -40,14 +41,17 @@ def convert_to_csv(profile):
         }
         writer.writerow(row)
 
-
-
-def main(): 
-    token = "EAAOo80pc1RABO1wF8BIwAQi9V7kPm5SojuPuFSGcsZCPSnaUbdNrPtjL0ikgIxYclUjpZBZA7vniqkfBrxLUOQirYtULBcxe4sHOZC71Tknsl0tsduw3SEQZBC5XVL40nQypLeo9KfCO1BvGf3YXn5LVWcy7IWTRaeuAKvdI5mPTVb9PM1ZATyqfvn9t7Xjyuijv43A8OGEUJB9i7JXVxnKTNFWL5SFVrvZAuoZBZBIBV2XZCV5ErWamNM2R5SFjRLKMAZD"
+def main(key): 
+    token = key
     
     graph = facebook.GraphAPI(token) 
     profile = graph.get_object('me', fields='first_name, gender, birthday, email, link, likes{category}')
-    print(profile)
-    convert_to_csv(profile)
-    
-main()
+    file= pd.read_csv("profile_dataNewest.csv")
+    if ((file['first_name'].eq(profile.get('first_name', '')).any()) & (file['email'].eq(profile.get('email', '')).any())):
+        print("user already exists")
+        
+    else:
+        convert_to_csv(profile)
+        print("Successfully updated the user")
+        
+main("EAAOo80pc1RABO3za5pOgYek939dXlTGT8rZCOe1Bv7qZCZAtKluVa0UDex35vQprtbOBkyTVTBqMDgZBvUfYJoQ1q0xgLOAWJVltrslV4cw1Tm7WuG1a7TGjjcrZABrvReZCvKAgexdktqsA7XOlwAuYhce4Vpb1WPG7lyflCjY6fPpqtVZBkiVIeQGi1QqKzaEj3xKn7xSYoluXHrH7zsZD")
